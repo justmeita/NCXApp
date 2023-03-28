f2239f9f89c42d506a6c00bad16ecc874b37490c
Letterio Ugo Cangiano <letterio@live.it>
2023-03-28 13:53:14 +0200

    Initial Commit

---
+++ NCXApp/NCXAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NCXAppApp.swift
+//  NCXApp
+//
+//  Created by Letterio Ugo Cangiano on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NCXAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
